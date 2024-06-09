2769005b3de4d83e9699dd9cd4ca57ce9e46739a
gladwinch <gladwinch20@gmail.com>
2024-06-09 11:13:08 +0530

    user schema added

--- src/schemas/User.schema.ts
+++ src/schemas/User.schema.ts
@@ -1,9 +1,56 @@
-import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 
-@Schema()
-export class User {
-  @Prop({ unique: true })
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  })
   email: string;
+
+  @Prop({ minlength: 7 })
+  password: string;
+
+  @Prop({ default: false })
+  isEmailConfirmed: boolean;
+
+  @Prop({ default: null })
+  fcmToken: string;
+
+  @Prop({ default: null })
+  avatar: string;
+
+  @Prop({ default: null })
+  googleId: string;
+
+  @Prop({ default: 'user' })
+  role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre<User>('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.matchPassword = function (enteredPassword: string) {
+  return bcrypt.compareSync(enteredPassword, this.password);
+};
